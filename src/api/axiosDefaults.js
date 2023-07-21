6b3171e91c892bbf10ff984afd408c80b88ec2ba
Neillcllghn <>
2023-07-21 08:42:48 +0000

    Installing axios and connecting react project to api

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://test-taskmanager-api-c8a395f1942c.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
