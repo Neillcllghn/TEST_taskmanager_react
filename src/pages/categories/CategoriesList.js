18fc80bd046bc7dab40991627d6e45db049f2172
Neillcllghn <>
2023-08-04 17:32:22 +0000

    create categoriesList.js to display the categories created

--- src/pages/categories/CategoriesList.js
+++ src/pages/categories/CategoriesList.js
@@ -1,14 +1,55 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosReq } from '../../api/axiosDefaults';
+
+import appStyles from "../../App.module.css"
+import Asset from '../../components/Assets';
+import Categories from './Categories';
 
 
 
-function CategoriesList() {
+function CategoriesList({message, filter=""}) {
+    const [category, setCategory] = useState({ result: [] });
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        const fetchCategory = async () => {
+            try {
+                const {data} = await axiosReq.get(`/category/${filter}`)
+                setCategory(data)
+                setHasLoaded(true)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+
+        setHasLoaded(false)
+        fetchCategory()
+    }, [filter, pathname])
+
   return (
     <Row className='h-100'>
         <Col className='py-2 p-0 p-lg-2' lg={8}>
             <p>List of Categories mobile</p>
-            <p>List of Categories displayed here</p>
+            {hasLoaded ? (
+                <>
+                {category.results.length ? (
+                    category.results.map(category => (
+                        <Categories key={category.id} {...category} setCategory={setCategory} />
+                    ))
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset message={message} />
+                    </Container>
+                )}
+                </>
+            ) : (
+                <Container className={appStyles.Content}>
+                    <Asset spinner />                     
+                    </Container>
+            )}
         </Col>
         <Col className='d-none d-lg-block p-0 p-lg-2' md={4}>
             <p>List of Categories desktop</p>
